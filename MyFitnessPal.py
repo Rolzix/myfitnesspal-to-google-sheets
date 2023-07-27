d277e5a99938e11659cbadbe9aae40851802b468
Rolzi <aki.peltoniemi@gmail.com>
2023-07-27 21:17:52 +0300

    Added Readme.md

--- MyFitnessPal.py
+++ MyFitnessPal.py
@@ -1,22 +1,33 @@
 import datetime
 import myfitnesspal
 import gspread
+import os
+import sys
 
 mfp = myfitnesspal.Client()
-sheets = gspread.service_account("./service_account.json")
-sh = sheets.open(
-    "HealthTracking"
-)  # Replace "HealthTracking" with the name of your spreadsheet
-worksheet = sh.worksheet("Sheet1")  # Replace "Sheet1" with the name of your worksheet
+
+# Get the path to the temporary folder containing the bundled files
+bundle_dir = getattr(sys, "_MEIPASS", os.path.abspath(os.path.dirname(__file__)))
+
+# Construct the path to the service account file
+service_account_file = os.path.join(bundle_dir, "service_account.json")
+
+# Load the service account credentials
+sheets = gspread.service_account(service_account_file)
+
+# Replace "HealthTracking" with the name of your spreadsheet
+sh = sheets.open("HealthTracking")
+
+# Replace "Sheet1" with the name of your worksheet
+worksheet = sh.worksheet("Sheet1")
 
 # Get the last date in the first column
-if len(worksheet.col_values(1)) < 1:  # Check if the last cell is empty
+if len(worksheet.col_values(1)) < 1:  # Check if the first cell is empty
     print("First cell is empty")
-    # worksheet.update(f"A1", [[1, 2], [3, 4]])  # Update the first cell with the date
     worksheet.update(
         f"A1", [["Date", "Total calories", "Carbohydrates", "Fat", "Protein"]]
     )  # Update the first row with the column names
-    worksheet.update(f"A2", "2023-01-01")  # Update the first cell with the date
+    worksheet.update(f"A2", "2023-01-01")  # Update the first data cell with the date
     last_date_str = worksheet.col_values(1)[-1]
 else:
     last_date_str = worksheet.col_values(1)[-1]
@@ -38,9 +49,10 @@ print(f"Starting from {start_date.strftime('%Y-%m-%d')}")
 end_date = datetime.date.today()
 print(f"Ending at {end_date.strftime('%Y-%m-%d')}")
 
-max_num_updated = 365  # Initialize the counter variable
+# Change this number to the maximum number of rows you want to update
+max_num_updated = 9999
 
-i = 0  # counter variable
+i = 1  # counter variable
 
 print("Starting loop")
 
@@ -48,24 +60,22 @@ for date in (
     start_date + datetime.timedelta(n)
     for n in range(0, (end_date - start_date).days + 1)
 ):
-    if (
-        max_num_updated == 0
-    ):  # Check if the number of updated rows is greater than or equal to 10
+    # Check if the number of updated rows is greater than max_num_updated
+    if max_num_updated < 1:
         print("Stopping because maximum number to update is reached")
         break  # Exit the loop if the condition is true
     print(f"Update operation number {i}")
-    # if (date != start_date+datetime.timedelta(i)):
-    #     print("Something is wrong, date is not equal to start_date")
-    #     print(f"Date is {date} and start_date is {start_date}")
-    #     break
+
     i += 1
 
     try:
+        # Get the MyFitnessPal data for the specified date
         day = mfp.get_date(date.year, date.month, date.day)
         print(f"Data from MyFitnessPal is {day}")
         print(
             f"Updating worksheet for {(date - start_date).days + start_index , 1, date.strftime('%Y-%m-%d')} <-- (row, col, date))"
         )
+        # Update the worksheet with the MyFitnessPal data for the current date
         worksheet.update(
             f"A{(date - start_date).days + start_index}",
             [
@@ -89,9 +99,13 @@ for date in (
     except:
         print(f"Something went wrong or no data for {date.strftime('%Y-%m-%d')}")
         print(f"Data from MyFitnessPal was {day}")
+        # Update the first cell with the date
         worksheet.update(
             f"A{(date - start_date).days + start_index}", [[date.strftime("%Y-%m-%d")]]
-        )  # Update the first cell with the date
+        )
 
 
 print(f"updated {i} rows, happy tracking!")
+
+
+# os.system("pause")
---
+++ README.md
@@ -0,0 +1,72 @@
+# MyFitnessPal to Google Sheets
+
+This Python script will take your MyFitnessPal data and put it into a Google Sheet.
+
+![demonstrationgif]()
+
+## How to use:
+
+### You need:
+
+- python-myfitnesspal ![python-myfitnesspal](https://github.com/coddingtonbear/python-myfitnesspal)
+
+```bash
+pip install myfitnesspal
+```
+
+- gspread ![gspread](https://github.com/burnash/gspread)
+
+```bash
+pip install gspread
+```
+
+### For the first time:
+
+- Follow the instructions on the gspread github page here :
+
+  https://docs.gspread.org/en/v5.10.0/oauth2.html#for-bots-using-service-account
+
+or watch this video here (first 2 minutes and 40 seconds is enough):
+
+![video](https://www.youtube.com/watch?v=bu5wXjz2KvU)
+
+- Copy the service account key JSON file into the same directory as the script.
+
+- Log in to MyFitnessPal in your browser. python-myfitnesspal will use your cookies for logging in.
+
+- Change the spreadsheet and sheet names to your own in these two places:
+
+```python
+# Replace "HealthTracking" with the name of your spreadsheet
+sh = sheets.open("HealthTracking")
+
+# Replace "Sheet1" with the name of your worksheet
+worksheet = sh.worksheet("Sheet1")
+```
+
+- Then run the script!
+
+### General usage tips:
+
+- The script will start updating the spreadsheet from the second to last row. So yesterdays data will always be updated.
+- If you miss more than 1 day, just delete the spreadsheet rows until the day you want to start updating from. The script will know which day to start from by reading the date on the last cell of the first column.
+
+## Future Improvements:
+
+- [] Change the script to use oauth2 instead of a service account.
+
+- [] Include standalone .exe to run the script without coding knowledge.
+
+  - [] Include variables to remember and change the spreadsheet and sheet names
+
+  - [] Include GUI
+
+## Lessons Learned:
+
+Notable Firsts:
+
+- First project with Python.
+-
+- First time using a Python library to access data on a website.
+-
+- First time using a Python library to access a Google API.
